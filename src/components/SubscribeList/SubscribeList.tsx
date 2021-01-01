da85e68f873a6ed3df7865230bf61bdd9d4db901
CHOI HEE JAE <gmlwo530@gmail.com>
2021-01-01 21:15:30 +0900

    feat: change dummy data functions

--- src/components/SubscribeList/SubscribeList.tsx
+++ src/components/SubscribeList/SubscribeList.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { Container, FullFlexContainer, SubscriptionList } from './style';
 import SubscribeItem from '@components/SubscribeItem';
 
-interface SubscribeItemData {
+interface Service {
   id: number;
 }
 
-const getDummyData = (): SubscribeItemData[] => {
-  const dd = [];
-  for (let i = 0; i < 10; i++) {
-    dd.push({ id: i });
-  }
-  return dd;
-};
-
 const SubscribeList = () => {
+  const [services, setServices] = useState<Service[] | null>(null);
+
+  const dummyServics: Service[] | null = useMemo(() => {
+    const dd = [];
+    for (let i = 0; i < 10; i++) {
+      dd.push({ id: i });
+    }
+    return dd;
+  }, [services]);
+
   return (
     <FullFlexContainer>
       <Container>
         <SubscriptionList
-          data={getDummyData()}
+          data={dummyServics}
           renderItem={({ item }) => <SubscribeItem id={item.id} />}
           keyExtractor={(item) => String(item.id)}
         />
